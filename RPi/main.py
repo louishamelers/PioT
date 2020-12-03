0a39ce6553ee54784017ebc06fd081553b1b4088
Tom Griebe <53947137+Tommus1997@users.noreply.github.com>
2020-12-03 17:02:31 +0100

    Decode message

--- RPi/main.py
+++ RPi/main.py
@@ -2,12 +2,14 @@ import ttn
 import time
 import binascii
 from config import access_key
+import base64
 
 app_id = "iot-plant-solution"
 
 def uplink_callback(msg, client):
   print("Received uplink from ", msg.dev_id)
-  print(msg.payload_raw)
+  payload = base64.b64decode(msg.payload_raw)
+  print(payload)
 
 handler = ttn.HandlerClient(app_id, access_key)
 
@@ -17,5 +19,5 @@ mqtt_client.set_uplink_callback(uplink_callback)
 mqtt_client.connect()
 
 print("Connected, waiting for uplink...")
-time.sleep(60)
+time.sleep(60 * 5)
 mqtt_client.close()
